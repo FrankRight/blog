ea18540d0d1455ece7f6f73111bd95661b815142
FrankRight <frankbosire2017@gmail.com>
2022-06-12 13:42:38 +0300

    homepage content

---
+++ src/pages/HomePage.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function HomePage(props) {
+  return (
+    <>
+      <h1>Hello, Welcome to my blog!</h1>
+      <p>
+        It wasn't that he hated her. It was simply that he didn't like her much.
+        It was difficult for him to explain this to her, and even more difficult
+        for her to truly understand. She was in love and wanted him to feel the
+        same way. He didn't, and no matter how he tried to explain to her she
+        refused to listen or to understand.
+      </p>
+      <p>
+        I haven't bailed on writing. Look, I'm generating a random paragraph at
+        this very moment in an attempt to get my writing back on track. I am
+        making an effort. I will start writing consistently again!
+      </p>
+    </>
+  );
+}
+
+export default HomePage;
